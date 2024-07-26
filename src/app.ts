fedc1fcf42a802152affeddfc9f605dc99679690
Mostafizur-Pro <mostafizur0195@gmail.com>
2024-07-26 17:42:52 +0600

    postman connect

--- src/app.ts
+++ src/app.ts
@@ -12,4 +12,12 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
+app.post("/", (req: Request, res: Response) => {
+  console.log(req.body);
+  // res.send('got data')
+  res.json({
+    message: "Successfully received data",
+  });
+});
+
 export default app;
